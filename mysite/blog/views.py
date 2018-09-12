f4d6cfb71acca23d6381636666a7ba3d5c9783ef
Julian <jaykmcclinton@gmail.com>
2018-09-11 20:22:57 -0400

    welcome everyone

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from  .forms import PostModelForm
 from django.http import HttpResponse
 
 from .models import PostModel
@@ -6,5 +7,23 @@ from .models import PostModel
 
 def post_model_list_view(request):
     qs = PostModel.objects.all()
-    print(qs)
-    return HttpResponse("some data")
+
+    template = "blog/list-view.html"
+    context = {
+        "object_list": qs,
+    }
+    return render(request, template, context)
+
+def post_model_create_view(request):
+    template = "blog/create-view.html"
+    form = PostModelForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        context = {
+            "form": PostModelForm()
+        }
+    return render(request, template, context)
